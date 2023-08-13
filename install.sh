c9fb7c1f5f401259d54ce92d3f773efc305a5319
Nino <137048437+Ninoskid@users.noreply.github.com>
2023-08-13 13:50:13 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+clear
+echo -e "${GREEN}Installing Proxy...${ENDCOLOR}"
+sleep 1
+if [ -f "nino" ]; then
+    echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
+    rm nino
+    sleep 1
+    echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
+fi
+wget -q https://github.com/Ninoskid/proxy/raw/main/nino
+sleep 1
+echo -e "${GREEN}Proxy Installed${ENDCOLOR}"
+echo -e "${GREEN}Execute proxy with this command: ./nino{ENDCOLOR}"
+chmod +x nino
